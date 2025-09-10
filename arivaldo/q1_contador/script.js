0dc7415875eb049d1ee428764df1e0e64e032c20
Arivaldo Araújo <147668194+Ariaol@users.noreply.github.com>
2025-09-09 21:31:05 -0300

    Add files via upload

---
+++ arivaldo/q1_contador/script.js
@@ -0,0 +1,29 @@
+let contador = 0;
+const valorContadorElemento = document.getElementById('valorContador');
+const btnDiminuir = document.getElementById('diminuir');
+const btnAumentar = document.getElementById('aumentar');
+const btnResetar = document.getElementById('resetar');
+
+// Atualiza o display do contador
+function atualizarContador() {
+  valorContadorElemento.textContent = contador;
+}
+
+// Funções para os botões
+btnDiminuir.addEventListener('click', () => {
+  contador--;
+  atualizarContador();
+});
+
+btnAumentar.addEventListener('click', () => {
+  contador++;
+  atualizarContador();
+});
+
+btnResetar.addEventListener('click', () => {
+  contador = 0;
+  atualizarContador();
+});
+
+// Chamada inicial para exibir o valor 0
+atualizarContador();
